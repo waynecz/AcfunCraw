b5b9db24c6665e3d8c937f9e6de43f4dbab7c5fd
Wayne <chengzhou.weng@fraudmetrix.cn>
2016-01-25 17:19:30 +0800

    clear&optimize

--- app.js
+++ app.js
@@ -1,57 +1,80 @@
-// 这句的意思就是引入 `express` 模块，并将它赋予 `express` 这个变量等待使用。
-var express = require('express');
 var cheerio = require('cheerio'); //引入cheerio
-var path = require('path');
 var fs = require('fs');
 var url = require('url');
 var request = require('request');
-// 调用 express 实例，它是一个函数，不带参数调用时，会返回一个 express 实例，将这个变量赋予 app 变量。
-var app = express();
+var eventproxy = require('eventproxy');
 
-// app 本身有很多方法，其中包括最常用的 get、post、put/patch、delete，在这里我们调用其中的 get 方法，为我们的 `/` 路径指定一个 handler 函数。
-// 这个 handler 函数会接收 req 和 res 两个对象，他们分别是请求的 request 和 response。
-// request 中包含了浏览器传来的各种信息，比如 query 啊，body 啊，headers 啊之类的，都可以通过 req 对象访问到。
-// res 对象，我们一般不从里面取信息，而是通过它来定制我们向浏览器输出的信息，比如 header 信息，比如想要向浏览器输出的内容。这里我们调用了它的 #send 方法，向浏览器输出一个字符串。
-
-var options = {
-	url: 'http://www.acfun.tv/v/list60/index.htm',
-	headers: {
-		'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'
-	},
-	gzip: true
+var ep = new eventproxy();
+var codes = ['60', '70', '68', '69', '59'];
+var dictionary = {
+	'60': '娱乐区',
+	'70': '科技区',
+	'68': '影视区',
+	'69': '体育区',
+	'59': '游戏区',
+	'60_': 'entertainment',
+	'70_': 'science',
+	'68_': 'movie',
+	'69_': 'sport',
+	'59_': 'game'
 };
 
-
-request(options, function(error, res, body) {
-	console.log(res.statusCode)
-	if (!error && res.statusCode == 200) {
-		// console.log(body); 
-		topics = [];
+ep.after('fetch_html', codes.length, function (responses) {
+	responses.map(function (response) {
+		var code = response[0];
+		var body = response[1];
+		var address = 'http://www.acfun.tv';
+		var topics = [];
 		var $ = cheerio.load(body);
 		$('#area-b .r .page.active .unit').each(function(i, e) {
 			var $e = $(e);
-			var o = {};
-			var href = $e.find('.r a').attr('href');
-			o.href = url.resolve(options.url, href);
-			o.text = $e.find('.r>a').text();
-			o.img = $e.find('.l img')[0].attribs['data-src'];
-			o.author = $e.find('.l img')[0].attribs['data-src'];
-			topics.push(o);
-			var fileName = parseUrlForFileName(o.img);
-			downloadImg(o.img, fileName)
+			var rst = {};
+			var link = $e.find('.r a').attr('href');
+			var authorLink = $e.find('.r>.author>a').attr('href');
+
+			rst.href = url.resolve(address, link);
+			rst.title = $e.find('.r>a').text();
+			rst.img = $e.find('.l img')[0].attribs['data-src'];
+			rst.authorLink = url.resolve(address, authorLink);
+			rst.authorText = $e.find('.r>.author>a').text();
+			rst.info = $e.find('.r>.info-extra').text().split('\n')[0];
+
+			topics.push(rst);
+			// var fileName = parseUrlForFileName(o.img);
+			// downloadImg(o.img, fileName);
 		})
+		var last = JSON.stringify(topics);
 
-		topics = JSON.stringify(topics);
-		fs.writeFile('../myNode/public/crawlerData/acfun.json', topics, function(err) {
+		fs.writeFile('../myNode/public/crawlerData/acfun-' + dictionary[code + '_'] + '.json', last, function(err) {
 			if (err) throw err;
-			console.log('json saved!');
+			console.log('AC ' + dictionary[code] + '榜单获取成功了 ( ゜- ゜)つロ');
 		});
-
-
-	} else {
-		console.log('出错啦！出错代码：' + res.statusCode)
-	}
+	})
 })
+
+codes.map(function (code) {
+	request({
+		url: 'http://www.acfun.tv/v/list' + code + '/index.htm', 
+		headers: {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'},
+		gzip: true
+	}, 
+	function(error, res, body) {
+		if (!error && res.statusCode == 200) {
+			// var thisUrl =  'http://www.acfun.tv/v/list' + code + '/index.htm';
+			console.log('对接 AC ' + dictionary[code] + ' 成功！');
+			ep.emit('fetch_html', [code, body]);
+		}
+		else {
+			console.log('对接 AC ' + dictionary[code] + '出错啦！出错代码：' + res.statusCode)
+		}
+	})
+});
+
+
+
+			
+		
+
 
 
 function parseUrlForFileName(address) {
@@ -59,21 +82,14 @@ function parseUrlForFileName(address) {
 	return fileName;
 }
 
-var downloadImg = function(uri, filename) {
-		request.head(uri, function(err, res, body) {
-
-			if (err) {
-				console.log('err:' + err);
-				return false;
-			}
-			request(uri).pipe(fs.createWriteStream('../myNode/public/images/' + filename)).on('close', function() {
-				console.log(filename + ' done!')
-			})
-
-
+function downloadImg(uri, filename) {
+	request.head(uri, function(err, res, body) {
+		if (err) {
+			console.log('err:' + err);
+			return false;
+		}
+		request(uri).pipe(fs.createWriteStream('../myNode/public/images/' + filename)).on('close', function() {
+			console.log(filename + ' done!')
 		})
-	}
-	// 定义好我们 app 的行为之后，让它监听本地的 3000 端口。这里的第二个函数是个回调函数，会在 listen 动作成功后执行，我们这里执行了一个命令行输出操作，告诉我们监听动作已完成。
-	// app.listen(4000, function() {
-	// 	console.log('app is listening at port 4000');
-	// });
+	})
+}
